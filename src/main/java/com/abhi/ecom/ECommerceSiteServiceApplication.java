094d0324dd24da7e6bae1a6d0f85fd9002cdcb11
abhij2109 <abhij2116@gmail.com>
2024-09-05 00:35:05 +0530

    Initial setup.

---
+++ src/main/java/com/abhi/ecom/ECommerceSiteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.abhi.ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceSiteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceSiteServiceApplication.class, args);
+	}
+
+}
