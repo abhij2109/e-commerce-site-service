2180dd436c79e7411eacd4ad11420e7c70fbd9a5
abhij2109 <abhij2116@gmail.com>
2024-10-26 00:05:54 +0530

    Added the getters, setters and constructors.

--- src/main/java/com/abhi/ecom/request/LoginRequest.java
+++ src/main/java/com/abhi/ecom/request/LoginRequest.java
@@ -1,5 +1,14 @@
 package com.abhi.ecom.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequest {
     private String userName;
     private String passWord;
