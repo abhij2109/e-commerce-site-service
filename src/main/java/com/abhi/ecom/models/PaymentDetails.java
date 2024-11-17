0a2e0536797bc3f7fff4cb087d3e78889fb89794
abhij2109 <abhij2116@gmail.com>
2024-11-18 00:46:51 +0530

    Added the PaymentDetail information for model.

---
+++ src/main/java/com/abhi/ecom/models/PaymentDetails.java
@@ -0,0 +1,18 @@
+package com.abhi.ecom.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentDetails {
+    private String paymentMethod;
+    private String status;
+    private String paymentId;
+    private String razorpayPaymentLinkId;
+    private String razorpayPaymentLinkReferenceId;
+    private String razorpayPaymentLinkStatus;
+    private String razorpayPaymentId;
+}
