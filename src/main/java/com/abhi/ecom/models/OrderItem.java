fbd7c3eec98e9d96c4376a47cb086f826e96a9fe
abhij2109 <abhij2116@gmail.com>
2024-11-18 00:24:07 +0530

    Added the fields in models.

--- src/main/java/com/abhi/ecom/models/OrderItem.java
+++ src/main/java/com/abhi/ecom/models/OrderItem.java
@@ -1,6 +1,30 @@
 package com.abhi.ecom.models;
 
-import jakarta.persistence.Entity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@RequiredArgsConstructor
+@NoArgsConstructor
 public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @JsonIgnore
+    @ManyToOne
+    private Order order;
+    @ManyToOne
+    private Product product;
+    private String size;
+    private Integer quantity;
+    private Integer price;
+    private Integer discountedPrice;
+    private Long userId;
+    private LocalDateTime deliveryDate;
 }
