4e0ea53633d911510900baafdae7b38db4a880c0
abhij2109 <abhij2116@gmail.com>
2024-11-19 00:47:02 +0530

    Working on OrderServiceImplementation.

---
+++ src/main/java/com/abhi/ecom/service/implementations/OrderServiceImplementation.java
@@ -0,0 +1,53 @@
+package com.abhi.ecom.service.implementations;
+
+import com.abhi.ecom.exceptions.OrderException;
+import com.abhi.ecom.models.Address;
+import com.abhi.ecom.models.Order;
+import com.abhi.ecom.models.User;
+import com.abhi.ecom.service.OrderService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderServiceImplementation implements OrderService {
+    @Override
+    public Order createOrder(User user, Address shippingAddress) {
+        return null;
+    }
+
+    @Override
+    public Order findOrderById(Long orderId) throws OrderException {
+        return null;
+    }
+
+    @Override
+    public List<Order> userOrdersHistory(Long userId) {
+        return null;
+    }
+
+    @Override
+    public Order placedOrder(Long orderId) throws OrderException {
+        return null;
+    }
+
+    @Override
+    public Order confirmedOrder(Long orderId) throws OrderException {
+        return null;
+    }
+
+    @Override
+    public Order shippedOrder(Long orderId) throws OrderException {
+        return null;
+    }
+
+    @Override
+    public Order deliveredOrder(Long orderId) throws OrderException {
+        return null;
+    }
+
+    @Override
+    public Order canceledOrder(Long orderId) throws OrderException {
+        return null;
+    }
+}
