7f67beb9516f8857b1c67b15a4fc81fcd6f8824a
abhij2109 <abhij2116@gmail.com>
2024-11-20 00:20:21 +0530

    Completed the cart item entity.

---
+++ src/main/java/com/abhi/ecom/models/CartItem.java
@@ -0,0 +1,29 @@
+package com.abhi.ecom.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JsonIgnore
+    @ManyToOne
+    private Cart cart;
+
+    @ManyToOne
+    private Product product;
+
+    private String size;
+    private int quantity;
+    private Integer price;
+}
