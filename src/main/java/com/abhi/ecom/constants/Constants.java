b6780e80df6a3592d537c5fce683b1d6dfe6e5d7
abhij2109 <abhij2116@gmail.com>
2024-11-20 18:12:56 +0530

    Added more constants values.

--- src/main/java/com/abhi/ecom/constants/Constants.java
+++ src/main/java/com/abhi/ecom/constants/Constants.java
@@ -5,4 +5,10 @@ public class Constants {
     public static final String JWT_HEADER = "Authorization";
     public static final String SINGUP_SUCCESSFUL = "SignUp Successfully";
     public static final String LOGIN_SUCCESSFUL = "LogIn Successful";
+    public static final String PRODUCT_NOT_FOUND = "Product not found with ID: ";
+    public static final String PRODUCT_DELETED_SUCCESS = "Product Deleted Successfully";
+    public static final String ITEM_ADDED_IN_CART = "Item Added to Cart.";
+    public static final String IN_STOCK = "In_stock";
+    public static final String OUT_OF_STOCK = "Out_of_stock";
+
 }
