84fd291705f90fae7c53f16076b0e60848136a87
abhij2109 <abhij2116@gmail.com>
2024-11-22 00:43:00 +0530

    WORK IN PROGRESS : more endpoints to add.

--- src/main/java/com/abhi/ecom/controller/ProductController.java
+++ src/main/java/com/abhi/ecom/controller/ProductController.java
@@ -1,7 +1,40 @@
 package com.abhi.ecom.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.abhi.ecom.exceptions.ProductException;
+import com.abhi.ecom.models.Product;
+import com.abhi.ecom.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/products")
 public class ProductController {
+
+    private final ProductService productService;
+    @GetMapping("/products")
+    public ResponseEntity<Page<Product>> findProductByCategoryHandler(
+            @RequestParam String category, @RequestParam List<String> color,
+            @RequestParam List<String> size, @RequestParam Integer minPrice,
+            @RequestParam Integer maxPrice, @RequestParam Integer minDiscount,
+            @RequestParam String sort, @RequestParam String stock,
+            @RequestParam Integer pageNumber, @RequestParam Integer pageSize
+            ){
+        Page<Product> result =
+                productService.getAllProduct(category, color, size, minPrice, maxPrice, minDiscount, sort, stock, pageNumber, pageSize);
+
+        return new ResponseEntity<>(result, HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("/product/{productId}")
+    public ResponseEntity<Product> findProductById(@PathVariable("productId") Long productId) throws ProductException{
+
+        Product product = productService.findProductById(productId);
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
 }
