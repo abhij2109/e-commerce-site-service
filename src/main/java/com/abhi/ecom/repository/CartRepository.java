630eb5ceef422d511fa8abd78f73d7628798a58e
abhij2109 <abhij2116@gmail.com>
2024-11-21 21:12:26 +0530

    Corrected the query as per JPA standards.

--- src/main/java/com/abhi/ecom/repository/CartRepository.java
+++ src/main/java/com/abhi/ecom/repository/CartRepository.java
@@ -4,9 +4,11 @@ import com.abhi.ecom.models.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
-    @Query("Select c from cart c where c.user.id = :userId")
-    Cart findByUserId(@Param("userId") Long userId);
+    @Query("Select c from Cart c where c.user.id = :userId")
+    public Cart findByUserId(@Param("userId") Long userId);
 }
