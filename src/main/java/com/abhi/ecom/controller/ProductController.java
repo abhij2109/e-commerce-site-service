ff0ab1cbddb3fc66e431f5d314253c314c8dece9
abhij2109 <abhij2116@gmail.com>
2024-11-24 17:43:38 +0530

    Fixed the URL and added some more functionality

--- src/main/java/com/abhi/ecom/controller/ProductController.java
+++ src/main/java/com/abhi/ecom/controller/ProductController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/products")
+@RequestMapping("/api")
 public class ProductController {
 
     private final ProductService productService;
