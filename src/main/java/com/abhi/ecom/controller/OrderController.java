3e534a569d654ad4465c4be120e073ff61ed2231
abhij2109 <abhij2116@gmail.com>
2024-11-24 17:46:19 +0530

    Adding orderHistory functionality of the user.

--- src/main/java/com/abhi/ecom/controller/OrderController.java
+++ src/main/java/com/abhi/ecom/controller/OrderController.java
@@ -1,5 +1,6 @@
 package com.abhi.ecom.controller;
 
+import com.abhi.ecom.exceptions.OrderException;
 import com.abhi.ecom.exceptions.UserException;
 import com.abhi.ecom.models.Address;
 import com.abhi.ecom.models.Order;
@@ -10,6 +11,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,5 +29,21 @@ public class OrderController {
         Order createdOrder = orderService.createOrder(user, shippingAddress);
 
         return new ResponseEntity<>(createdOrder, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/user/")
+    public ResponseEntity<List<Order>> getOrderHistory(@RequestHeader("Authorization") String jwt) throws UserException{
+
+        User user = userService.findUserByJwt(jwt);
+        List<Order> orders = orderService.userOrdersHistory(user.getUserId());
+
+        return new ResponseEntity<>(orders, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}/")
+    public ResponseEntity<Order> getOrderById(@PathVariable("id") Long orderId,
+                                              @RequestHeader("Authorization") String jwt) throws OrderException{
+        Order order = orderService.findOrderById(orderId);
+        return new ResponseEntity<>(order, HttpStatus.OK);
     }
 }
